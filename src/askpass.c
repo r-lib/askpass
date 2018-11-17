e4d792daf06f6d5c0139b6f0d8ea2f239442a5b5
Jeroen Ooms <jeroenooms@gmail.com>
2018-11-17 15:32:30 +0100

    Please CMD check

--- src/askpass.c
+++ src/askpass.c
@@ -1,6 +1,8 @@
 #include <Rinternals.h>
 #include <unistd.h>
 
+/* We don't use this anymore */
+
 SEXP pw_entry_dialog(SEXP prompt){
 #ifndef _WIN32
   const char *text = CHAR(STRING_ELT(prompt, 0));
@@ -9,4 +11,15 @@ SEXP pw_entry_dialog(SEXP prompt){
     return Rf_mkString(pass);
 #endif
   return R_NilValue;
+}
+
+static const R_CallMethodDef CallEntries[] = {
+  {"pw_entry_dialog", (DL_FUNC) &pw_entry_dialog, 1},
+  {NULL, NULL, 0}
+};
+
+void R_init_askpass(DllInfo *dll){
+  R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+  R_useDynamicSymbols(dll, FALSE);
+  R_forceSymbols(dll, TRUE);
 }
