d6a51fe42c7f26c3b2a6b965f91f459aba1b20c7
Jeroen Ooms <jeroenooms@gmail.com>
2018-11-17 15:24:58 +0100

    Simplify

--- src/askpass.c
+++ src/askpass.c
@@ -1,17 +1,12 @@
 #include <Rinternals.h>
 #include <unistd.h>
 
-static SEXP safe_char(const char *x){
-  if(x == NULL)
-    return NA_STRING;
-  return Rf_mkCharCE(x, CE_UTF8);
-}
-
 SEXP pw_entry_dialog(SEXP prompt){
-  const char *text = CHAR(STRING_ELT(prompt, 0));
 #ifndef _WIN32
-  return Rf_ScalarString(safe_char(getpass(text)));
-#else
-  return R_NilValue;
+  const char *text = CHAR(STRING_ELT(prompt, 0));
+  const char *pass = getpass(text);
+  if(pass != NULL)
+    return Rf_mkString(pass);
 #endif
+  return R_NilValue;
 }
