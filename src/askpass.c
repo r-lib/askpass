1368456a3d678e19fd005d268830fddb6082fecc
Jeroen Ooms <jeroenooms@gmail.com>
2018-11-17 14:11:33 +0100

    Make it compile on Windows

--- src/askpass.c
+++ src/askpass.c
@@ -10,5 +10,9 @@ static SEXP safe_char(const char *x){
 
 SEXP pw_entry_dialog(SEXP prompt){
   const char *text = CHAR(STRING_ELT(prompt, 0));
+#ifndef _WIN32
   return Rf_ScalarString(safe_char(getpass(text)));
+#else
+  return R_NilValue;
+#endif
 }
