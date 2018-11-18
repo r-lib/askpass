b5c8748be0a1454ed5a672d625d26d45450f4604
Jeroen Ooms <jeroenooms@gmail.com>
2018-11-19 00:23:23 +0100

    Small tweaks

--- R/onload.R
+++ R/onload.R
@@ -3,16 +3,16 @@
 }
 
 setup_askpass_vars <- function(){
-  if(nchar(Sys.getenv('RSTUDIO'))){
+  if(var_exists('RSTUDIO')){
     fix_rstudio_path()
   } else {
     # This is mostly for RGui and R.app (tty could mean MacOS server)
     if(is_windows() || (is_macos() && !isatty(stdin()))){
       askpass_bin = ssh_askpass()
-      if(is.na(Sys.getenv('GIT_ASKPASS', NA))){
+      if(!var_exists('GIT_ASKPASS')){
         Sys.setenv("GIT_ASKPASS" = askpass_bin)
       }
-      if(is.na(Sys.getenv('SSH_ASKPASS', NA))){
+      if(!var_exists('SSH_ASKPASS')){
         Sys.setenv("SSH_ASKPASS" = askpass_bin)
       }
     }
@@ -23,7 +23,7 @@ setup_askpass_vars <- function(){
 # See: https://github.com/rstudio/rstudio/issues/3805
 fix_rstudio_path <- function(){
   rs_path <- Sys.getenv('RS_RPOSTBACK_PATH')
-  git_askpass <- Sys.getenv('GIT_ASKPASS', 'rpostback-askpass')
+  git_askpass <- Sys.getenv('GIT_ASKPASS')
   if(nchar(rs_path) && !cmd_exists(git_askpass)){
     PATH <- Sys.getenv("PATH")
     if(!grepl(normalizePath(rs_path, mustWork = FALSE), PATH, fixed = TRUE)){
@@ -32,6 +32,10 @@ fix_rstudio_path <- function(){
                               collapse = .Platform$path.sep))
     }
   }
+}
+
+var_exists <- function(var){
+  nchar(Sys.getenv(var)) > 0
 }
 
 cmd_exists <- function(cmd){
